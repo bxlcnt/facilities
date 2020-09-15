855e4794534e401192f2a0bc6b4dc21ee908b151
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-09-15 13:20:10 -0700

    auto

--- lcntProc.sh
+++ lcntProc.sh
@@ -2,13 +2,17 @@
 
 typeset RcsId="$Id: lcntProc.sh,v 1.3 2019-04-22 17:46:30 lsipusr Exp $"
 
-####+BEGIN: bx:dblock:lsip:bash:seed-spec :types "seedLcntProc.sh"
-# {{{ DBLOCK-seed-spec
-if [ "${loadFiles}X" == "X" ] ; then
-    /opt/public/osmt/bin/seedLcntProc.sh -l $0 "$@" 
+####+BEGIN: bx:bsip:bash:seed-spec :types "seedLcntProc.sh"
+SEED="
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedLcntProc.sh]] | 
+"
+FILE="
+*  /This File/ :: /bisos/git/auth/bxRepos/bxlcnt/facilities/lcntProc.sh 
+"
+if [ "${loadFiles}" == "" ] ; then
+    /bisos/core/bsip/bin/seedLcntProc.sh -l $0 "$@" 
     exit $?
 fi
-# }}} DBLOCK-seed-spec
 ####+END:
 
 ####+BEGIN: bx:dblock:lcnt:bash:dirs-list :types ""
